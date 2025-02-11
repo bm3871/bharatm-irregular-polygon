2aef10f8c9d13e8c43527cf356dd67b79a809942
bm3871 <bm3871@pleasantonusd.net>
2025-02-11 22:43:33 +0000

    first commit, add point and perimeter methods

--- IrregularPolygon.java
+++ IrregularPolygon.java
@@ -14,12 +14,18 @@ public class IrregularPolygon {
     // public methods
     public void add(Point2D.Double aPoint)
     {
-        // TODO: Add a point to the IrregularPolygon.
+        myPolygon.add(aPoint);
     }
 
     public double perimeter() {
-        // TODO: Calculate the perimeter.
-        return 3.14;
+        if (myPolygon.size() < 2) return 0.0;
+        double perimeter = 0.0;
+        for (int i = 0; i < myPolygon.size(); i++) {
+            Point2D.Double p1 = myPolygon.get(i);
+            Point2D.Double p2 = myPolygon.get((i + 1) % myPolygon.size());
+            perimeter += p1.distance(p2);
+        }
+        return perimeter;
     }
 
     public double area() {
