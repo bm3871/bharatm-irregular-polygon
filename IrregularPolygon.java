82e0c621adcf06dead1c7fdda81043416dcac2b8
bm3871 <bm3871@pleasantonusd.net>
2025-02-13 21:44:30 +0000

    area method

--- IrregularPolygon.java
+++ IrregularPolygon.java
@@ -29,8 +29,15 @@ public class IrregularPolygon {
     }
 
     public double area() {
-        // TODO: Calculate the area.
-        return 0.0;
+        if (myPolygon.size() < 3) return 0.0;
+        double sum1 = 0.0, sum2 = 0.0;
+        for (int i = 0; i < myPolygon.size(); i++) {
+            Point2D.Double p1 = myPolygon.get(i);
+            Point2D.Double p2 = myPolygon.get((i + 1) % myPolygon.size());
+            sum1 += p1.x * p2.y;
+            sum2 += p1.y * p2.x;
+        }
+        return Math.abs(sum1 - sum2) / 2.0;
     }
 
     public void draw()
